f24ed950e7ef3bac7d590181433f257533b08446
NATKAMONLAK <paintlpch@gmail.com>
2021-03-12 15:44:10 +0700

    completed

--- lab22_1.cpp
+++ lab22_1.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
 
-
 class Node{
     public:
     	int data;
@@ -63,70 +62,23 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
-#include<iostream>
-using namespace std;
-
-
-class Node{
-    public:
-    	int data;
-    	Node *next;
-    	~Node();
-};
-
-class List{
-	public:
-		Node *root;
-		int size;
-		void show();
-		void append(int);
-		void insert(int,int);
-		void remove(int);
-};
-
-Node::~Node(){
-    cout << data << " was deleted.\n";
-}
-
-void List::insert(int d,int idx){	
+void List::remove(int idx){
 	Node *n = new Node;
-	n->data = d;
-	
+
 	if(idx == 0){
-		n->next = root;
-		root = n;
-		return;
+		n = root;
+		root = root->next;
+		delete n;
+		size --;
 	}
-	Node *current = root;
-	for(int i = 0; i < idx-1;i++){
-		current = current->next;
-	}
-	n->next = current->next;
-	current->next = n;		
-}
-
-void List::show(){
-	Node *current = root;
-	cout << current->data << " ";	
-	while(current->next){
-		current = current->next;
-		cout << current->data << " ";
-	}	
-}
-
-void List::append(int d){	
-	Node *n = new Node;
-	n->data = d; n->next = NULL;
-	if(root == NULL) root = n;
 	else{
-		Node *current = root;
-		while(current->next){
-			current = current->next;
-		}
-		current->next = n;
+	Node *current = root;
+	for(int i=0; i<idx-1; i++){
+		current = current->next;
 	}
-	size++;
+	n = current->next;
+	current->next = current->next->next;
+	delete n;
+	size--;
+	}
 }
-
-//Write List::remove() here
